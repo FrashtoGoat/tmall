7a52eccb6104c688ac97977b38b115d07ef969b0
FrashtoGoat <1271573554@qq.com>
2021-08-09 23:37:00 +0800

    springboot-elasticjob整合

--- tmall-demo-elasticjob/src/main/java/com/xiaoluban/TmallDemoElasticjobApplication.java
+++ tmall-demo-elasticjob/src/main/java/com/xiaoluban/TmallDemoElasticjobApplication.java
@@ -1,15 +1,13 @@
 package com.xiaoluban;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author txb
- * @date 2021/1/25 16:49
+ * @date 2021/8/9
  */
 @SpringBootApplication
-@MapperScan("com.xiaoluban.**.**.mapper")
 public class TmallDemoElasticjobApplication {
 
     public static void main(String[] args) {
